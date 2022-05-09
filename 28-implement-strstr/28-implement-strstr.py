c3df6238ff675965d289fa3e3b683e970fd942e1
Matiyas Yohannes <73067967+Matiyas1994@users.noreply.github.com>
2022-05-09 17:16:12 +0300

    Time: 45 ms (55.97%), Space: 13.8 MB (79.05%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.py
+++ 28-implement-strstr/28-implement-strstr.py
@@ -3,19 +3,17 @@ class Solution:
         if needle =="":
             return 0
         
-        if needle in haystack:
-            return haystack.index(needle)
+        Nh, Nn = len(haystack), len(needle)
+        
+        for i in range(Nh-Nn+1):  
+            if haystack[i] == needle[0]:
+                if haystack[i:].startswith(needle):
+                    return i
         return -1
+        
+        
+        # if needle in haystack:
+        #     return haystack.index(needle)
+        # return -1
     
-#         p1 = 0
-#         p2 = len(needle)
-        
-#         while p2 < len(needle):
-#             if haystack[p1:p2+1] == needle:
-#                 return p1
-#             p1 +=1
-#             p2 +=1
-            
-#         return -1
-        
         
