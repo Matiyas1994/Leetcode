6c466899cd3c782143924f92235000eae80f4fa0
Matiyas Yohannes <73067967+Matiyas1994@users.noreply.github.com>
2022-05-21 00:07:51 +0300

    Time: 33 ms (90.85%), Space: 14.2 MB (33.85%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,16 +1,30 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
+#         res = []
+#         n = len(nums)
+        
+#         for i in range(1<<n):
+#             curlist = []
+#             for j in range(n-1,-1,-1):
+#                 if i&(1<<j):
+#                     curlist.append(nums[j])
+#             res.append(curlist)
+        
+#         return res
+        
+    
         res = []
-        n = len(nums)
-        
-        for i in range(1<<n):
-            curlist = []
-            for j in range(n-1,-1,-1):
-                if i&(1<<j):
-                    curlist.append(nums[j])
-            res.append(curlist)
-        
+        def rec(cur,i):
+            if i >= len(nums):
+                res.append(cur[:])
+                return
+
+            rec(cur[:],i+1)
+            cur.append(nums[i])
+            rec(cur[:],i+1)
+        rec([],0)
         return res
+
         
         
         
