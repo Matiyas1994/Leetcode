0e18ff832dcd13a91bd4dead4069af434ac4a1f6
Matiyas Yohannes <73067967+Matiyas1994@users.noreply.github.com>
2022-05-28 12:37:18 +0300

    Time: 218 ms (37.10%), Space: 15.3 MB (34.52%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -1,10 +1,17 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-#         n = len(nums) + 1
-#         s = 0
-#         for num in nums:
-#             s = s ^ num
-#         return n - s
+
+    #   """  BIt Manipulation with XOR """
+    #  eg [3,0,4,1] = 4 ^ 0 ^ 3 ^ 1 ^ 0 ^ 2 ^ 4 ^ 3 ^ 1 = 2     
+    
+        res = len(nums) 
+        for idx,num in enumerate(nums):
+            res ^= num
+            res ^= idx
+        return res
+
+
+    #   """ Using sort """
         # nums.sort()
         # numsLength = len(nums)
         # if nums[-1] != numsLength:
@@ -12,7 +19,9 @@ class Solution:
         # for idx, val in enumerate(nums):
         #     if idx != val:
         #         return idx
-        n = len(nums)
-        sumOneUpToN = n * (n + 1)//2
-        sumOfTheArray = sum(nums)
-        return sumOneUpToN - sumOfTheArray
+        
+        """Using Maths"""
+        # n = len(nums)
+        # sumOneUpToN = n * (n + 1)//2
+        # sumOfTheArray = sum(nums)
+        # return sumOneUpToN - sumOfTheArray
