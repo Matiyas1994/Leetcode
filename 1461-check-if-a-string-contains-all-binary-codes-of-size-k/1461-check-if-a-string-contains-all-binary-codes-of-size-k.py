425327cb712500de580fb7d470ab0cebc3f0d1e0
Matiyas Yohannes <73067967+Matiyas1994@users.noreply.github.com>
2022-05-31 21:03:18 +0300

    Time: 325 ms (93.36%), Space: 27.1 MB (51.77%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.py
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.py
@@ -1,18 +1,33 @@
 class Solution:
     def hasAllCodes(self, s: str, k: int) -> bool:
-        _len = len(bin((2**k)-1)) - 2
-        _set = set()
+        reqNumberOfSubsets = (2**k) 
+        seen = set()
+        for i in range(len(s)- k+1):
+            curStr = s[i: i + k]
+            if curStr not in seen:
+                seen.add(curStr)
+                reqNumberOfSubsets -=1
+            if  reqNumberOfSubsets == 0:
+                return True
+        return False
         
-        for i in range(len(s)):
-            _set.add(s[i:i + _len])
         
-        for i in range(2**k):
-            b = bin(i) 
-            curLen = len(b) - 2
-            _str = "0" * (_len - curLen) + b[2:] 
-            if _str not in _set:
-                return False
+        
+        
+        
+#         _len = len(bin((2**k)-1)) - 2
+#         _set = set()
+        
+#         for i in range(len(s)):
+#             _set.add(s[i:i + _len])
+        
+#         for i in range(2**k):
+#             b = bin(i) 
+#             curLen = len(b) - 2
+#             _str = "0" * (_len - curLen) + b[2:] 
+#             if _str not in _set:
+#                 return False
             
-        return True
+#         return True
         
         
