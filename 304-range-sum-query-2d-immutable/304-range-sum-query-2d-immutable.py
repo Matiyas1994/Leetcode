f665cda4907129c9336c64f467e2a0f846f92f60
Matiyas Yohannes <73067967+Matiyas1994@users.noreply.github.com>
2022-06-03 10:54:24 +0300

    Time: 2072 ms (53.26%), Space: 26.6 MB (6.78%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.py
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.py
@@ -2,25 +2,25 @@ class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
         self.matrix = matrix
-        self.prefix_matrix = []
+        self.dp = [[0 for i in range(len(matrix[0])+1)] 
+                   for j in range(len(matrix)+1)]
         
-        # PreProcessing the matrix by building prefix sum         
-        for i in range(len(matrix)):
-            temp = [0]
-            for j in range(len(matrix[0])):
-                temp.append(matrix[i][j] + temp[-1])
-                
-            self.prefix_matrix.append(temp)
-        
+        # Buiiding comulative sum relative to the orgin for each cell
+        for r in range(len(matrix)):
+            for c in range(len(matrix[0])):
+                self.dp[r+1][c+1] = (self.matrix[r][c]
+                                     +self.dp[r][c+1]
+                                     +self.dp[r+1][c]
+                                     -self.dp[r][c])
+    
     
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        sum_of_bounded_region = 0
-        for i in range(row1,row2+1):
-            sum_of_bounded_region += (self.prefix_matrix[i][col2+1]
-                                      -self.prefix_matrix[i][col1])
-                
-        return sum_of_bounded_region
+        dp = self.dp
+        
+        return (dp[row2+1][col2+1] - dp[row2+1][col1]
+                - dp[row1][col2+1] + dp[row1][col1]) 
 
 # Your NumMatrix object will be instantiated and called as such:
 # obj = NumMatrix(matrix)
 # param_1 = obj.sumRegion(row1,col1,row2,col2)
+
