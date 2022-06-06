51e835bed5b10ef663aa43531b063a3966550525
Matiyas Yohannes <73067967+Matiyas1994@users.noreply.github.com>
2022-06-06 09:49:39 +0300

    Time: 187 ms (62.32%), Space: 29.5 MB (69.35%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.py
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.py
@@ -8,30 +8,51 @@ class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
         p1 = headA
         p2 = headB
-        c = 0
-        c2 = 0
-        while p1:
-            c += 1
-            p1 = p1.next
-            
-        while p2:
-            c2 +=1
-            p2 = p2.next
-        p1 = headA
-        p2 = headB
         
-        if c <= c2:
-            for _ in range(c2-c):
-                p2 = p2.next
-        else:
-            for _ in range(c-c2):
+        while p1 != p2:
+            if p1 == None:
+                p1 = headB
+            else:
                 p1 = p1.next
+            
+            if p2 == None:
+                p2 = headA
+            else:
+                p2 = p2.next
         
-        while p1 and p2:
-            if p1 == p2:
-                return p1
-            p1 = p1.next
-            p2 = p2.next
+        return p1
+        
+        
+        
+        
+        
+        
+#         p1 = headA
+#         p2 = headB
+#         c = 0
+#         c2 = 0
+#         while p1:
+#             c += 1
+#             p1 = p1.next
+            
+#         while p2:
+#             c2 +=1
+#             p2 = p2.next
+#         p1 = headA
+#         p2 = headB
+        
+#         if c <= c2:
+#             for _ in range(c2-c):
+#                 p2 = p2.next
+#         else:
+#             for _ in range(c-c2):
+#                 p1 = p1.next
+        
+#         while p1 and p2:
+#             if p1 == p2:
+#                 return p1
+#             p1 = p1.next
+#             p2 = p2.next
             
             
         
