1c7c78af4c536bf9233280ca4aa6e79255774d21
Matiyas Yohannes <73067967+Matiyas1994@users.noreply.github.com>
2022-06-07 09:43:51 +0300

    Time: 52 ms (48.48%), Space: 14 MB (37.34%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -3,30 +3,52 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
+        p1 =m-1
+        p2 =n-1
+        i = m+n - 1
+        
+        while p1>=0 and p2>=0:
+            if nums1[p1] > nums2[p2]:
+                nums1[i] = nums1[p1]
+                p1, i = p1-1, i-1
+                
+            else:
+                nums1[i] = nums2[p2]
+                p2, i = p2-1, i-1
+            
+        
+        while p2 >= 0:
+            nums1[i] = nums2[p2]
+            p2, i = p2-1, i-1
+            
+        
+        
+#################################################################
         # k=0
         # for i in range(m,m+n):
         #     nums1[i]=nums2[k]
         #     k+=1
         # nums1.sort()
         
-        c = []
-        p1 = 0
-        p2 = 0
+#####################################################################        
+#         c = []
+#         p1 = 0
+#         p2 = 0
         
-        while p1 < m and p2 < n:
-            if nums1[p1] <= nums2[p2]:
-                c.append(nums1[p1])
-                p1 +=1
-            else:
-                c.append(nums2[p2])
-                p2 +=1
-        if p1 < m:
-            c.extend(nums1[p1:m])
-        if p2 < n:
-            c.extend(nums2[p2:n])
+#         while p1 < m and p2 < n:
+#             if nums1[p1] <= nums2[p2]:
+#                 c.append(nums1[p1])
+#                 p1 +=1
+#             else:
+#                 c.append(nums2[p2])
+#                 p2 +=1
+#         if p1 < m:
+#             c.extend(nums1[p1:m])
+#         if p2 < n:
+#             c.extend(nums2[p2:n])
             
-        for i in range(m+n):
-            nums1[i] = c[i]
+#         for i in range(m+n):
+#             nums1[i] = c[i]
             
         
 
