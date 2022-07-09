20671bcc87d2bb336f1b5a0e4cf531f2a3563d48
Matiyas Yohannes <73067967+Matiyas1994@users.noreply.github.com>
2022-07-10 00:35:46 +0300

    Time: 15732 ms (5.00%), Space: 58 MB (5.01%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -1,16 +1,36 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        memo = {}
-        def minijump(idx = 0):
-            if idx >= len(nums)-1:
-                return 0
-            if idx in memo:
-                return memo[idx]
-            res = float(inf)
-            for i in range(1,nums[idx]+1):
-                res = min(minijump(idx + i),res)
-            memo[idx] =  res + 1
-            return memo[idx]
+        n = len(nums)
+        dp=[float("inf") for _ in range(n)]
+        dp[0]=0
+        canreach = defaultdict(list)
         
-        return minijump()
+        for k in range(1,nums[0]+1):
+                canreach[k].append(0)
+        
+        for i in range(1,n):
+            for j in canreach[i]:
+                dp[i] = min(dp[i], dp[j]+1)
+            for k in range(1,nums[i]+1):
+                canreach[i+k].append(i)
+            
+        return dp[-1]
+                
+        
+        
+        
+#         memo = {}
+#         def minijump(idx = 0):
+#             if idx >= len(nums)-1:
+#                 return 0
+#             if idx in memo:
+#                 return memo[idx]
+#             res = float(inf)
+#             for i in range(1,nums[idx]+1):
+#                 res = min(minijump(idx + i),res)
+#             memo[idx] =  res + 1
+#             return memo[idx]
+        
+#         return minijump()
+        
         
