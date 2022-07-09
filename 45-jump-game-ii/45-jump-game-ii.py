b4a2d9e287defe52cb4489d1b9c357cbcf690775
Matiyas Yohannes <73067967+Matiyas1994@users.noreply.github.com>
2022-07-10 01:44:55 +0300

    Time: 130 ms (96.81%), Space: 15 MB (58.79%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -1,15 +1,37 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
         n = len(nums)
-        dp=[float("inf") for _ in range(n)]
-        dp[-1]=0
+        if n==1:
+            return 0
+        can_reach = -1
+        interval = [0,0]
+        jumps = 0
         
-        for i in range(n-2,-1,-1):
-            for j in range(1,nums[i]+1):
-                if j+i <= n-1:
-                    dp[i] = min(dp[i], dp[i+j]+1)
+        while True:
+            jumps +=1
+            for i in range(interval[0],interval[1]+1):
+                can_reach = max(can_reach, nums[i]+i)
             
-        return dp[0]
+            if can_reach >= n-1:
+                return jumps
+            
+            interval[0] = interval[1]+1
+            interval[1] = can_reach
+
+        
+        
+        
+        
+#         n = len(nums)
+#         dp=[float("inf") for _ in range(n)]
+#         dp[-1]=0
+        
+#         for i in range(n-2,-1,-1):
+#             for j in range(1,nums[i]+1):
+#                 if j+i <= n-1:
+#                     dp[i] = min(dp[i], dp[i+j]+1)
+            
+#         return dp[0]
                 
         
         
