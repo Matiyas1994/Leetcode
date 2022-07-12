68ee024441383b1a9854c1d855b966749d4bbc7d
Matiyas Yohannes <73067967+Matiyas1994@users.noreply.github.com>
2022-07-13 01:24:18 +0300

    Time: 50 ms (55.65%), Space: 13.8 MB (97.26%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -3,15 +3,38 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        colorsFreq = [0,0,0]
+        s, e, i = 0, len(nums)-1, 0
         
-        for n in nums:
-            colorsFreq[n] +=1
+        while i <= e:
+            if nums[i]==0:
+                nums[i], nums[s] = nums[s], 0
+                s +=1
+                i +=1
+            elif nums[i]==2:
+                nums[i], nums[e] = nums[e], 2
+                e -=1
+            else:
+                i +=1
+                
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         colorsFreq = [0,0,0]
+        
+#         for n in nums:
+#             colorsFreq[n] +=1
     
-        start = 0
-        for i in range(3):
-            for j in range(start, start+colorsFreq[i]):
-                nums[j] = i
-            if colorsFreq[i] > 0: 
-                start = j+1 
+#         start = 0
+#         for i in range(3):
+#             for j in range(start, start+colorsFreq[i]):
+#                 nums[j] = i
+#             if colorsFreq[i] > 0: 
+#                 start = j+1 
         
