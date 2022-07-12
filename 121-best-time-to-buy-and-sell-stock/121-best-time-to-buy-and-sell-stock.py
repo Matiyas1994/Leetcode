cdcb0e80f32a1199dd4614998d7c4f39bb62b450
Matiyas Yohannes <73067967+Matiyas1994@users.noreply.github.com>
2022-07-13 02:01:12 +0300

    Time: 2092 ms (12.65%), Space: 25.1 MB (37.97%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,15 +1,11 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        max_so_far = []
-        for i,n in enumerate(prices):
-            heapq.heappush(max_so_far,(-1*n,i))
-        
         max_profit = 0
-        for i in range(len(prices)-1):
-            while max_so_far[0][1] <= i:
-                heapq.heappop(max_so_far)
-            max_profit = max(max_profit, -1*max_so_far[0][0] - prices[i])
+        min_price_so_far = float(inf)
+        
+        for i in range(len(prices)):
+            min_price_so_far = min(min_price_so_far, prices[i])
+            max_profit = max(max_profit, prices[i] - min_price_so_far)
         
         return max_profit
-        
         
