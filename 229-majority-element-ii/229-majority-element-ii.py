c87c4317f2e7281640246b1021dce4938c893811
Matiyas Yohannes <73067967+Matiyas1994@users.noreply.github.com>
2022-07-15 23:27:01 +0300

    Time: 138 ms (77.06%), Space: 15.4 MB (24.45%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.py
+++ 229-majority-element-ii/229-majority-element-ii.py
@@ -1,41 +1,36 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        limit = n//3
         candidate1, candidate2 = None, None
         count1, count2 = 0, 0
         
         for num in nums:
-            if candidate1 != None and num == candidate1:
+            if candidate1 !=None and candidate1 == num:
                 count1 +=1
-            elif candidate2 != None and num == candidate2:
+            elif candidate2 !=None and  candidate2 == num:
                 count2 +=1
-            
-            elif count1 == 0:
+            elif count1==0:
                 candidate1 = num
-                count1 +=1
-            elif count2 == 0:
+                count1 = 1
+            elif count2==0:
                 candidate2 = num
-                count2 +=1
+                count2 = 1
             else:
                 count1 -=1
                 count2 -=1
-        
-        limit = len(nums)//3
-        res = []
-        count1, count2 =0, 0
+                
+        count1, count2 = 0, 0
         for num in nums:
             if num == candidate1:
                 count1 +=1
             elif num == candidate2:
                 count2 +=1
-            
+        ans = []
         if count1 > limit:
-            res.append(candidate1)
+            ans.append(candidate1)
         if count2 > limit:
-            res.append(candidate2) 
+            ans.append(candidate2)
+        return ans
         
-        return res
-                
-                
-            
         
-         
