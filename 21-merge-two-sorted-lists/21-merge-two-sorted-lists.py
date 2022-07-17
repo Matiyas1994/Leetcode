ce5b95954e940a1f8939059232825cff24264993
Matiyas Yohannes <73067967+Matiyas1994@users.noreply.github.com>
2022-07-18 01:26:06 +0300

    Time: 58 ms (50.17%), Space: 13.9 MB (32.33%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -5,36 +5,43 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        head = None
-        if not list1:
-            return list2
-        if not list2:
-            return list1
-        if list1.val <= list2.val:
-            temp = list1
-            L1 = list1.next
-            L2 = list2
-            head = list1
-        else:
-            temp = list2
-            L2 = list2.next
-            L1 = list1
-            head = list2
+        if not list1: return list2
+        if not list2: return list1
         
-        while L1 and L2:
-            if L1.val <= L2.val:
-                temp.next = L1
-                temp = L1
-                L1 = L1.next
-            else:
-                temp.next = L2
-                temp = L2
-                L2 = L2.next
-        if L1:
-            temp.next = L1
-        if L2:
-            temp.next = L2
+        if list1.val > list2.val:
+            list1, list2 = list2, list1
+            
+        head = list1
+        
+        while list1 and list2:
+            while list1 and list1.val <= list2.val:
+                temp = list1
+                list1 = list1.next
+            temp.next = list2
+            list1, list2 = list2, list1
         
         return head
+        
+        
+        
+#         head = ListNode()
+#         temp = head
+#         L1, L2 = list1, list2
+        
+#         while L1 and L2:
+#             if L1.val <= L2.val:
+#                 temp.next = L1
+#                 temp = L1
+#                 L1 = L1.next
+#             else:
+#                 temp.next = L2
+#                 temp = L2
+#                 L2 = L2.next
+#         if L1:
+#             temp.next = L1
+#         if L2:
+#             temp.next = L2
+        
+#         return head.next
         
         
