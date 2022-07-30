77714c8e1629111182137c3502c705d6538b33ca
Matiyas Yohannes <73067967+Matiyas1994@users.noreply.github.com>
2022-07-30 16:48:45 +0300

    Time: 65 ms (46.55%), Space: 14.1 MB (22.26%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,17 +1,41 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
+        """
+        with constant space
+        """
         ans = []
-        def permutationGenerator(start=0, temp=[]):
-            if len(temp)== len(nums):
-                ans.append(temp[:])
+        def backtrack(start,nums):
+            if start == len(nums):
+                ans.append(nums[:])
             
             else:
-                for i in range(len(nums)):
-                    if nums[i] in temp: continue
-                    temp.append(nums[i])
-                    permutationGenerator(i+1,temp)
-                    temp.pop()
-                    
-        permutationGenerator()
+                for i in range(start,len(nums)):
+                    nums[start], nums[i] = nums[i], nums[start]
+                    backtrack(start+1,nums)
+                    nums[start], nums[i] = nums[i], nums[start]
+                        
+        backtrack(0,nums)
         return ans
+        
+        
+        
+        
+        
+#         ans = []
+#         exist = [0 for i in range(len(nums))]
+#         def permutationGenerator(start=0, temp=[], exist=exist):
+#             if len(temp)== len(nums):
+#                 ans.append(temp[:])
+            
+#             else:
+#                 for i in range(len(nums)):
+#                     if exist[i] !=0 : continue
+#                     temp.append(nums[i])
+#                     exist[i] = 1
+#                     permutationGenerator(i+1,temp, exist)
+#                     temp.pop()
+#                     exist[i] = 0
+                    
+#         permutationGenerator()
+#         return ans
         
