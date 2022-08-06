25d8b9429b0a7b65eca4875e89dc8b090375ead5
Matiyas Yohannes <73067967+Matiyas1994@users.noreply.github.com>
2022-08-06 15:51:22 +0300

    Time: 38 ms (76.64%), Space: 13.9 MB (74.45%) - LeetHub

--- 458-poor-pigs/458-poor-pigs.py
+++ 458-poor-pigs/458-poor-pigs.py
@@ -1,17 +1,17 @@
 class Solution:
     def poorPigs(self, buckets: int, minutesToDie: int, minutesToTest: int) -> int:
-        l, r = 0, buckets
-        T = (minutesToTest//minutesToDie)+1
-        ans = buckets
-        while l<=r:
-            mid = l + (r-l)//2
-            if pow(T, mid) >= buckets:
-                ans = mid
-                r = mid-1
-            else:
-                l +=1
+        return math.ceil(math.log(buckets,(minutesToTest//minutesToDie)+1))
         
-        return ans
+#         l, r = 0, buckets
+#         T = (minutesToTest//minutesToDie)+1
+#         ans = buckets
+#         while l<=r:
+#             mid = l + (r-l)//2
+#             if pow(T, mid) >= buckets:
+#                 ans = mid
+#                 r = mid-1
+#             else:
+#                 l +=1
+        
+#         return ans
             
-        
-        
