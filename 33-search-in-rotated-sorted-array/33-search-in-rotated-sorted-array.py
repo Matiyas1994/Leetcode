19a38a74336047d24773f0d01b9dfaa962d0c9a8
Matiyas Yohannes <73067967+Matiyas1994@users.noreply.github.com>
2022-08-07 12:41:36 +0300

    Time: 66 ms (49.94%), Space: 14.3 MB (56.29%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -1,74 +1,26 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        def bs(left,right,target):
-            while left<= right:
-                mid = left + (right-left)//2
-                
-                if nums[mid]== target:
-                    return mid
-                elif nums[mid] < target:
+        left, right = 0, len(nums)-1
+        
+        while left <= right:
+            mid = left + (right-left)//2
+            
+            if nums[mid]== target:
+                return mid
+            
+            if nums[left] <= nums[mid]:
+                if nums[left]<= target <= nums[mid]:
+                    right = mid-1
+                else:
+                    left = mid+1
+            else:
+                if nums[mid]<= target <= nums[right]:
                     left = mid+1
                 else:
                     right = mid-1
-                
-            return -1
-        
-        if nums[0] > nums[-1]:
-            left, right =0, len(nums)-1
-            pivot = right
-            while left<= right:
-                mid = left + (right-left)//2
-                
-                if nums[mid] < nums[0]:
-                    pivot = mid
-                    right = mid-1
-                else:
-                    left = mid + 1
                     
-            if target == nums[pivot]:
-                return pivot
-            
-            if target <= nums[-1]:
-                return bs(pivot,len(nums)-1, target)
-            else:
-                return bs(0, pivot-1, target)
-            
-        else:
-            return bs(0,len(nums)-1,target)
-            
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        return -1
+                
         
 #         def bs(L,R,targ):
 #             while L <= R:
