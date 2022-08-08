415bf9ae9c014a54e11f2c9568eef8428dad9899
Matiyas Yohannes <73067967+Matiyas1994@users.noreply.github.com>
2022-08-08 10:16:56 +0300

    Time: 151 ms (77.33%), Space: 14.3 MB (47.45%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
@@ -1,14 +1,47 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        dp = [ 1 for i in range(len(nums))]
-        ans = 1
-        
+        def firstGreatorElement(ds, targ):
+            left, right = 0, len(ds)-1
+            candidate = right
+            while left<=right:
+                mid = left + (right-left)//2
+                
+                if ds[mid] >= targ:
+                    candidate = mid
+                    right = mid-1
+                else:
+                    left = mid+1
+            
+            return candidate
+        ds = [nums[0]]
         for i in range(1, len(nums)):
-            for j in range(i-1, -1,-1):
-                if nums[j] < nums[i]:
-                    dp[i] = max(dp[j]+1, dp[i])
-            ans = max(ans, dp[i])
-        print(dp)
-        return ans
+            if nums[i] > ds[-1]:
+                ds.append(nums[i])
+            else:
+                idx = firstGreatorElement(ds, nums[i])
+                ds[idx] = nums[i]
+        
+        return len(ds)
+                
+            
+            
+            
+        
+        
+        
+        
+        
+        
+        
+#         dp = [ 1 for i in range(len(nums))]
+#         ans = 1
+        
+#         for i in range(1, len(nums)):
+#             for j in range(i-1, -1,-1):
+#                 if nums[j] < nums[i]:
+#                     dp[i] = max(dp[j]+1, dp[i])
+#             ans = max(ans, dp[i])
+
+#         return ans
         
         
