373b3ee1fa058cf45b9eb09a117d967483a8605f
Matiyas Yohannes <73067967+Matiyas1994@users.noreply.github.com>
2022-08-09 10:30:42 +0300

    Time: 387 ms (7.19%), Space: 20.3 MB (83.03%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
@@ -1,42 +1,53 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-#         def colFindFor(targ):
-#             left, right = 0, len(matrix)
-#             ans = right
-#             while left<=right:
-#                 mid = left + (right-left)//2
-                
-#                 if matrix[mid][0] <= targ:
-#                     ans = mid
-#                     left = mid+1
-#                 else:
-#                     right = mid-1
-            
-#             return ans
-        def bs (row, targ):
-            left, right = 0, len(matrix[0])-1
-            
-            while left<=right:
-                mid = left + (right-left)//2
-                
-                if matrix[row][mid] == targ:
-                    return True
-                
-                elif matrix[row][mid] > targ:
-                    right = mid-1
-                else:
-                    left = mid+1
-                          
-            return False
-        
         
         if matrix[0][0] <= target <= matrix[-1][-1]:
-            for row in range(len(matrix)):
-                if bs(row, target): return True
+            row, col = 0, len(matrix[0])-1
             
+            while col>=0 and row < len(matrix):
+                if matrix[row][col] == target:
+                    return True
+                elif matrix[row][col] > target:
+                    col -=1
+                else:
+                    row +=1
             return False
         else:
             return False
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         def bs (row, targ):
+#             left, right = 0, len(matrix[0])-1
+            
+#             while left<=right:
+#                 mid = left + (right-left)//2
+                
+#                 if matrix[row][mid] == targ:
+#                     return True
+                
+#                 elif matrix[row][mid] > targ:
+#                     right = mid-1
+#                 else:
+#                     left = mid+1
+                          
+#             return False
+        
+        
+#         if matrix[0][0] <= target <= matrix[-1][-1]:
+#             for row in range(len(matrix)):
+#                 if bs(row, target): return True
+            
+#             return False
+#         else:
+#             return False
         
         
       
