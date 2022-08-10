36445627e547ae89e4f466a616625ae3efbc6e04
Matiyas Yohannes <73067967+Matiyas1994@users.noreply.github.com>
2022-08-10 12:13:46 +0300

    Time: 3450 ms (5.00%), Space: 27.1 MB (14.18%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -1,31 +1,31 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        
-        def partion(L,R,pivot):
-            while R < pivot:
-                if nums[R] < nums[pivot]:
-                    nums[R], nums[L] = nums[L], nums[R]
+        def partion(L,R):
+            pivot = nums[R]
+
+            for j in range(L,R):
+                if nums[j] <= pivot:
+                    nums[j], nums[L] = nums[L], nums[j]
                     L +=1
                     
-                R +=1
-            nums[L], nums[pivot] = nums[pivot], nums[L]
+            nums[L], nums[R] = nums[R], nums[L]
             return L
-        
-        left, right, pivot = 0,0,len(nums)-1
-        
-        while True:
-            idx = partion(left,right,pivot)
             
-            if idx == len(nums)-k:
-                return nums[idx]
-            
+
+        left, right= 0, len(nums)-1
+        kth = len(nums)-k
+        
+        while left < right:
+            idx = partion(left,right)    
+            if idx == kth:
+                break
+                
             elif idx < len(nums)-k:
                 left = idx+1
-                right = idx+1
             else:
-                pivot = idx-1
-                right = 0
-                left = 0
+                right = idx-1
+                
+        return nums[kth]
             
             
             
