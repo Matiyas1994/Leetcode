93d5b16da55ca8641069b6276ca8d0345b327481
Matiyas Yohannes <73067967+Matiyas1994@users.noreply.github.com>
2022-08-19 10:56:38 +0300

    Time: 118 ms (66.90%), Space: 15.5 MB (48.44%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,40 +1,34 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        if len(nums) == 0:
-            return [-1,-1] 
+        def bs(flag):
+            L, R =0, len(nums)-1
             
-        Lans = -1
-        L = 0
-        R = len(nums) - 1
-        
-        while L<=R:
-            mid = L + (R-L)//2
+            ans =-1
             
-            if nums[mid] == target:
-                Lans = mid
-                R = mid -1
-            elif nums[mid] > target:
-                R = mid - 1
-            else:
-                L = mid + 1
-        
-        if Lans == - 1:
-            return [-1,-1]
-         
-        L = Lans
-        R = len(nums) - 1
-        Rans = Lans
-        while L<=R:
-            mid = L + (R-L)//2
-            
-            if nums[mid] == target: 
-                Rans = mid 
-                L = mid + 1
-            elif nums[mid] > target:
-                R = mid - 1
-        
-        return [Lans,Rans]
+            while L<=R:
+                mid = L + (R-L)//2
                 
+                if nums[mid]==target:
+                    ans = mid
+                    if flag:
+                        R = mid-1
+                    else:
+                        L = mid+1
+                elif nums[mid] > target:
+                    R = mid-1
+                else:
+                    L = mid+1
+                    
+            return ans
+    
+        return [bs(True), bs(False)]
+        
                 
-        
-        
+            
+            
+            
+            
+            
+                    
+            
+            
