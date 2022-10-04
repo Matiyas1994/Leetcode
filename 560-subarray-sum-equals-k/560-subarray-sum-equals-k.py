e4324b1a862e531afd417ecd5eb54bd4ef501d48
Matiyas Yohannes <73067967+Matiyas1994@users.noreply.github.com>
2022-10-04 21:56:55 +0300

    Time: 658 ms (35.09%), Space: 16.6 MB (33.71%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
@@ -1,59 +1,30 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        Myhash = {}
-        prefix_sum = 0
-        subarrays =0
+      #   [1,2,4,6,-5,4,1] k = 10
+      # [0 1 3,7,13,8,12,13]
+      #               13-10
+    
+     
+        prefixSum , ans= 0, 0
+        HashMap = {0 : 1}
+        n = len(nums)
         
-        for num in nums:
-            prefix_sum +=num
         
-            if  prefix_sum == k:
-                subarrays += 1
+        for i in range(n):
+            prefixSum += nums[i]
+          
+        
+            if prefixSum - k in HashMap:
+                ans += HashMap[prefixSum - k ] 
             
-            if prefix_sum - k in Myhash:
-                subarrays += Myhash[prefix_sum-k]
+            HashMap[prefixSum] = HashMap.get(prefixSum , 0) + 1
             
-            if prefix_sum in Myhash:
-                Myhash[prefix_sum] +=1
+     
                 
-            else:
-                Myhash[prefix_sum] =1 
-        
-        return subarrays
-        
-        
-        '''
-        p1 = 0
-        p2 = 0
-        presum = [0]
-        ans = 0
-        
-        if k == 0:
-            presum.append(nums[0])
-            p1 = 1
-        
-        while p1 <= len(nums)-1:
-            if nums[p1] == k:
-                ans += 1
-                p1 += 1
-            elif nums[p1] > k:
-                p1 +=1
-                presum.append(0)
-            
-            else:
-                p2 = p1
-                
-                while p2 <= len(nums)-1 and presum[-1] < k: 
-                    presum.append(presum[-1]+nums[p2])
-                    p2 +=1
-                    
-                if presum[-1] == k:
-                    ans +=1
-                    
-            
-                presum.append(0)    
-                p1 +=1
                 
         return ans
-        '''
+            
+        
+        
+        
         
