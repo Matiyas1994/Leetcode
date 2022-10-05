2935769d578a5639c240035fdf7b5f930df345cf
Matiyas Yohannes <73067967+Matiyas1994@users.noreply.github.com>
2022-10-05 11:45:36 +0300

    Time: 708 ms (76.81%), Space: 71.3 MB (51.59%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.py
+++ 115-distinct-subsequences/115-distinct-subsequences.py
@@ -1,25 +1,26 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
         
-        memo = {}
-        def f(i, j):
-            if j < 0: return 1
-            
-            if i < 0: return 0
-            
-    
-            if (i,j) in memo:
-                return memo[(i, j)]
-            
-            if s[i]==t[j]:
-                memo[(i, j)] = f(i-1, j-1) + f(i-1, j)
-            
-            else:
-                memo[(i, j)] = f(i-1, j)
-            
-            return memo[(i, j)]
+        n, m = len(s), len(t)
+
+        dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
         
-        i, j = len(s)-1, len(t)-1
-        return f(i, j)
-            
+        for i in range(n+1):
+            dp[i][0] = 1
+       
         
+        for i in range(1, n+1):
+               for j in range(1, m+1):
+               
+                    if s[i-1]==t[j-1]:
+                        dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
+               
+                    else:
+                        dp[i][j] = dp[i-1][j]
+              
+        return dp[n][m]
+               
+               
+                    
+        
+       
