c054f3a431a677a336881e31b22044aabd217ff0
Matiyas Yohannes <73067967+Matiyas1994@users.noreply.github.com>
2022-10-07 22:18:59 +0300

    Time: 3907 ms (23.78%), Space: 28.2 MB (79.61%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.py
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.py
@@ -1,20 +1,41 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        memo = {}
-        def f(i,canIbuy,n):
-            if i==len(prices) or n==0: return 0
-            
-            if (i, canIbuy, n) in memo:
-                return memo[(i, canIbuy, n)]
-            
-            if canIbuy:
-                profit = max(-prices[i]+f(i+1,0,n), f(i+1,1,n))
-                
-            else:
-                profit = max(prices[i]+f(i+1,1,n-1), f(i+1,0,n))
-            
-            memo[(i, canIbuy, n)] = profit
-            return memo[(i, canIbuy, n)]
+        #  t       f
+        # 1 2     1 2
         
-        return f(0,1,2)
+        ast1, ast2, asf1, asf2 = 0,0,0,0
+        for i in range(len(prices)-1,-1,-1):
+            for canIbuy in range(2):
+                for n in range(1,3):
+                     if canIbuy:
+                        if n==1: 
+                            ast1 = max(-prices[i]+asf1, ast1)
+                        else:
+                            ast2 = max(-prices[i]+ asf2, ast2)
+                            
+                     else:
+                        if n ==1:
+                            asf1 = max(prices[i], asf1)
+                        else:
+                            asf2 = max(prices[i]+ast1, asf2)
+                        
+        return ast2            
+        
+#         memo = {}
+#         def f(i,canIbuy,n):
+#             if i==len(prices) or n==0: return 0
+            
+#             if (i, canIbuy, n) in memo:
+#                 return memo[(i, canIbuy, n)]
+            
+#             if canIbuy:
+#                 profit = max(-prices[i]+f(i+1,0,n), f(i+1,1,n))
+                
+#             else:
+#                 profit = max(prices[i]+f(i+1,1,n-1), f(i+1,0,n))
+            
+#             memo[(i, canIbuy, n)] = profit
+#             return memo[(i, canIbuy, n)]
+        
+#         return f(0,1,2)
         
