23631121008ea233eb06d72ef6c984c6573002f0
Matiyas Yohannes <73067967+Matiyas1994@users.noreply.github.com>
2022-11-02 09:24:14 +0300

    Time: 54 ms (76.92%), Space: 13.9 MB (79.70%) - LeetHub

--- 0376-wiggle-subsequence/0376-wiggle-subsequence.py
+++ 0376-wiggle-subsequence/0376-wiggle-subsequence.py
@@ -1,21 +1,22 @@
 class Solution:
     def wiggleMaxLength(self, nums: List[int]) -> int:
-        relation = []
         n = len(nums)
         if n < 2:
             return n
+        ans = 0
+        relation = None
         for i in range(1,n):
             if nums[i] > nums[i-1]:
-                relation.append(1)
+                if relation == -1:
+                    ans +=1
+                relation = 1 
             elif nums[i] < nums[i-1]:
-                relation.append(-1)
-        k = len(relation)
-        ans = 0
-        if not relation:
+                if relation == 1:
+                    ans +=1
+                relation = -1
+                
+        if relation == None:
             return 1
-        for j in range(1,k):
-            if relation[j] != relation[j-1]:
-                ans +=1
-        return ans + 2 
+        return ans + 2  
         
         
