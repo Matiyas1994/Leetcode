3dd2c19601c8248209ce8ec3f0689cd0aa6e4f43
Matiyas Yohannes <73067967+Matiyas1994@users.noreply.github.com>
2022-11-09 01:38:56 +0300

    Time: 230 ms (91.65%), Space: 14.5 MB (9.11%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.py
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.py
@@ -6,7 +6,7 @@ class Solution:
         for r in range(n): 
             dic[s[r]] = dic.get(s[r], 0) + 1
             
-            while len(dic) >=3 and dic[s[l]] - 1 > 0:
+            while dic[s[l]] - 1 > 0:
                 dic[s[l]] -=1
                 l +=1
             
