094a8320ee41e674cfb5b7e279a9fecb69601ad9
Matiyas Yohannes <73067967+Matiyas1994@users.noreply.github.com>
2022-11-30 10:16:13 +0300

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.py
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.py
@@ -1,60 +1,27 @@
 
-from collections import defaultdict
+
 from typing import List
-class UnionFind:
-    def __init__(self, numNode):
-        self.parent = {i: i for i in range(numNode)}
-    
-    def find(self, node):
-        if self.parent[node] == node:
-            return node
-        self.parent[node] = self.find(self.parent[node])
-        return self.parent[node]
-        
-    def union(self, node1, node2):
-        node1 = self.find(node1)
-        node2 = self.find(node2)
-        
-        if node1 != node2:
-            self.parent[node2] = node1
-        
 class Solution:
     #Function to detect cycle in an undirected graph.
 	def isCycle(self, V: int, adj: List[List[int]]) -> bool:
-        parent = {}
-        visited = set()
-        
-        for i in range(V):
-            parent[i] = i
-            
-        def find(node):
-            if parent[node] == node:
-                return node
-            parent[node] = find(parent[node])
-            
-            return parent[node]
-        
-        def union(u, v):
-            root1, root2 = find(u), find(v)
-            
-            if root1 != root2:
-                parent[root1] = root2
-                return False
-            else:
-                return True
-		
-		for u,v  in enumerate(adj):
-		    for i in range(len(v)):
-		        if (u, v[i]) not in visited:
-                    points = union(u, v[i])
-                    visited.add((u, v[i]))
-                    visited.add((v[i], u))
-                    if points:
-                        return True
-        return False
-		        
+		#Code here
+		visited = set()
+		def cycledetect(node, previousNode, visited):
+		  
+		    visited.add(node)
 		    
-		
+		    for neigbour in adj[node]:
+		        if neigbour in visited and neigbour==previousNode: continue
+		        if neigbour in visited: return True
+		        else:
+		            if cycledetect(neigbour, node, visited): return True
+		            
+		for node in range(V):
+		    if node not in visited:
+    		    if cycledetect(node,-1, visited):
+    		        return True
+		return False
+		            
 
 
 #{ 
