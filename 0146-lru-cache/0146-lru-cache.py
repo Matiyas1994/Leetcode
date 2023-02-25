c1914e64761991fe78a9906c4636decb3f2a3920
Matiyas Yohannes <73067967+Matiyas1994@users.noreply.github.com>
2023-02-25 21:42:40 +0300

    Time: 846 ms (74.17%), Space: 75.7 MB (49.31%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,59 +1,70 @@
-class DLL:
-    def __init__(self, key=0, val=0, nxt=None, prev=None):
+class Node:
+    def __init__(self, key, val, nxt=None, prev = None):
         self.key = key
         self.val = val
         self.next = nxt
         self.prev = prev
-    
+        
+
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.numToLinkedList = {}
         self.capacity = capacity
-        self.Head = DLL()
-        self.Tail = DLL(0, 0, None, self.Head)
-        self.Head.next = self.Tail
+        self.dic = {}
+        self.head = Node(-1,-1)
+        self.tail = Node(-1,-1)
+        
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        
         
 
     def get(self, key: int) -> int:
-        # print(self.numToLinkedList)
-        if key in self.numToLinkedList:
-            node = self.numToLinkedList[key]
-            # newNode = DLL(node.key, node.val)
-            self.deleteNode(node)
-            self.addInTheFront(node)
-            
-            return node.val
-            
-        else:
+        if key not in self.dic:
             return -1
+        
+        node = self.dic[key]
+        result = node.val
+        self.deleteNode(node)
+        self.addNextToHead(node)
+        
+        return result
+        
 
     def put(self, key: int, value: int) -> None:
-        if key in self.numToLinkedList:
-            node = self.numToLinkedList[key]
-            node.val = value
+        if key in self.dic:
+            node = self.dic[key]
             self.deleteNode(node)
-            self.addInTheFront(node)
+            self.addNextToHead(node)
+            node.val = value
         else:
-            newNode = DLL(key, value)
-            if len(self.numToLinkedList) >= self.capacity:
-                deletedNode = self.Tail.prev
-                self.deleteNode(deletedNode)
-                del self.numToLinkedList[deletedNode.key]
-            self.numToLinkedList[key] = newNode
-            self.addInTheFront(newNode)
-
-        # print(self.numToLinkedList)
-    def addInTheFront(self, node):
-        node.next = self.Head.next
-        node.prev = self.Head
-        self.Head.next = node
-        node.next.prev = node
+            if len(self.dic) >= self.capacity:
+                delNode = self.tail.prev
+                self.deleteNode(delNode)
+                del self.dic[delNode.key]
+            
+            newNode = Node(key, value)
+            self.addNextToHead(newNode)
+            self.dic[key] = newNode
+            
+        
+        
     
-    def deleteNode(self, node):
+    def addNextToHead(self,node):
+        temp = self.head.next
+        self.head.next = node
+        node.next = temp
+        
+        temp.prev = node
+        node.prev = self.head
+        
+    
+    def deleteNode(self,node):
         node.prev.next = node.next
         node.next.prev = node.prev
-    
+        
+        
+
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
