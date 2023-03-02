c95016bb100b0b6f61eeffaf65f4e27326467ced
Matiyas Yohannes <73067967+Matiyas1994@users.noreply.github.com>
2023-03-02 15:31:10 +0300

    Time: 27 ms (88.73%), Space: 13.8 MB (49.42%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
@@ -1,3 +1,14 @@
 class Solution:
     def numTrees(self, n: int) -> int:
-        return (math.factorial(2*n))//(math.factorial(n+1)*(math.factorial(n)))
+        dp  = {0:1, 1:1, 2:2}
+        for i in range(1, n+1):
+            ans = 0
+            for j in range(1,i+1):
+                ans += dp[j-1] * dp[i-j] 
+            dp[i] = ans
+            
+            
+        return ans
+            
+        
+        
